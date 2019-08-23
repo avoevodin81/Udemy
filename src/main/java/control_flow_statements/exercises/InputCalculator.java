17c0a7f446bf6580caeae8ae66421b9f82814cb2
Andrii Voievodin <avoievodin@eisgroup.com>
2019-08-23 11:58:58 +0300

    Input Calculator impl

--- src/main/java/control_flow_statements/exercises/InputCalculator.java
+++ src/main/java/control_flow_statements/exercises/InputCalculator.java
@@ -1,4 +1,30 @@
 package control_flow_statements.exercises;
 
+import java.util.Scanner;
+
 public class InputCalculator {
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int sum = 0;
+        long avg = 0;
+        while (true) {
+            String s = scanner.next();
+            count++;
+            try {
+                sum += Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                if (sum != 0) {
+                    avg = Math.round((double) sum / --count);
+                }
+                System.out.println(String.format("SUM = %s AVG = %s", sum, avg));
+                scanner.close();
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
 }
