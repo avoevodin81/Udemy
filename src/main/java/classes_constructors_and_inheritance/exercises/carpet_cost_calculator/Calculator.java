0a3c4fbbd8de38bea9caa831109586cc9a2983cc
avoevodin81 <avoevodin81@gmail.com>
2019-08-25 20:03:48 +0300

    carpet_cost_calculator impl

---
+++ src/main/java/classes_constructors_and_inheritance/exercises/carpet_cost_calculator/Calculator.java
@@ -0,0 +1,22 @@
+package classes_constructors_and_inheritance.exercises.carpet_cost_calculator;
+
+public class Calculator {
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet) {
+        this.floor = floor;
+        this.carpet = carpet;
+    }
+
+    public double getTotalCost() {
+        return floor.getArea() * carpet.getCost();
+    }
+
+    public static void main(String[] args) {
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println(calculator.getTotalCost());
+    }
+}
