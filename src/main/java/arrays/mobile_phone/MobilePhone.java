2e1d776fc177f0f596218125d95992d88d001a4c
avoevodin81 <avoevodin81@gmail.com>
2019-08-29 20:37:47 +0300

    ArrayListChallenge finish

--- src/main/java/arrays/mobile_phone/MobilePhone.java
+++ src/main/java/arrays/mobile_phone/MobilePhone.java
@@ -23,13 +23,63 @@ public class MobilePhone {
         return true;
     }
 
+    public boolean updateContact(Contact oldContact, Contact newContact) {
+        int foundPosition = findContact(oldContact);
+        if (foundPosition < 0) {
+            System.out.println(oldContact.getName() + ", was not found.");
+            return false;
+        } else if (findContact(newContact.getName()) != -1) {
+            System.out.println("Contact with name: " + newContact.getName() + " already exists. Update was not successful.");
+            return false;
+        }
+
+        this.myContacts.set(foundPosition, newContact);
+        System.out.println(oldContact.getName() + ", was replaced with " + newContact.getName());
+        return true;
+    }
+
+    public boolean removeContact(Contact contact) {
+        int foundPosition = findContact(contact);
+        if (foundPosition < 0) {
+            System.out.println(contact.getName() + ", was not found.");
+            return false;
+        }
+        this.myContacts.remove(foundPosition);
+        System.out.println(contact.getName() + ", was deleted.");
+        return true;
+    }
+
     private int findContact(Contact contact) {
         return this.myContacts.indexOf(contact);
     }
 
     private int findContact(String contactName) {
         OptionalInt optionalInt = IntStream.range(0, myContacts.size())
-                .filter(i -> contactName.equals(myContacts.get(i))).findFirst();
+                .filter(i -> contactName.equals(myContacts.get(i).getName())).findFirst();
         return optionalInt.orElse(-1);
+    }
+
+    public String queryContact(Contact contact) {
+        if (findContact(contact) >= 0) {
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public Contact queryContact(String name) {
+        int position = findContact(name);
+        if (position >= 0) {
+            return this.myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List");
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i + 1) + "." +
+                    this.myContacts.get(i).getName() + " -> " +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
     }
 }
