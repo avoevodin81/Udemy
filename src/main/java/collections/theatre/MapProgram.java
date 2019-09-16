c56df9a4a611c1997a86be672634b9f28e41b78d
avoevodin81 <avoevodin81@gmail.com>
2019-09-16 16:45:11 +0300

    theatre impl

---
+++ src/main/java/collections/theatre/MapProgram.java
@@ -0,0 +1,11 @@
+package collections.theatre;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+        languages.put("Java", "a compiled high level, object-oriented, platform independent language");
+    }
+}
