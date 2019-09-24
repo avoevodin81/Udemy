367737cbeb94d503cfeb1802b48773cec8eed065
avoevodin81 <avoevodin81@gmail.com>
2019-09-24 17:35:00 +0300

    HeavenlyBody start impl

---
+++ src/main/java/collections/set/planets/HeavenlyBody.java
@@ -0,0 +1,61 @@
+package collections.set.planets;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public final class HeavenlyBody {
+    private final String name;
+    private final double orbitalPeriod;
+    private final Set<HeavenlyBody> satellites;
+
+    public HeavenlyBody(String name, double orbitalPeriod) {
+        this.name = name;
+        this.orbitalPeriod = orbitalPeriod;
+        this.satellites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public Set<HeavenlyBody> getSatellites() {
+        return new HashSet<>(satellites);
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satellites.add(moon);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        System.out.println("obj.getClass() is: " + obj.getClass());
+        System.out.println("this.getClass() is: " + this.getClass());
+
+        if (obj == null || getClass() != obj.getClass()) return false;
+        HeavenlyBody that = (HeavenlyBody) obj;
+        return Objects.equals(name, that.name);
+    }
+
+
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        HeavenlyBody that = (HeavenlyBody) o;
+//        return Objects.equals(name, that.name);
+//    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
