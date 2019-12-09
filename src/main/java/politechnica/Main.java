9031e26758c27f5dc3ee56631f046a6052839747
avoievodin <avoievodin@eisgroup.com>
2019-12-09 18:24:22 +0200

    politechnica main class update

--- src/main/java/politechnica/Main.java
+++ src/main/java/politechnica/Main.java
@@ -4,18 +4,34 @@ import static java.lang.Math.*;
 
 public class Main {
     public static void main(String[] args) {
-        float xMin, xMax, deltaX, a, step;
+        print(sort(new int[]{9, 5, 8, 1, 0}));
+    }
 
-        xMin = 15;
-        xMax = 200;
-        deltaX = abs(xMax - xMin);
-        step = deltaX / 10;
-
-        for (float i = xMin; i <= xMax; i+=step) {
-            System.out.println(i);
+    public static int[] sort(int[] mas) {
+        for (int i = 0; i < mas.length; i++) {
+            //сохраняем во временную переменную текущий элемент массива
+            int temp = mas[i];
+            //создаем индекс для прохода по отсортированной части
+            int j = i - 1;
+            //проверяем что индекс сортированной части массива в диапазоне
+            //проверяем что элемент с текущим индексом больше элемента
+            // из неотсортированной части
+            while (j >= 0 && mas[j] > temp) {
+                //присваевам значение следующему элементу текущего,
+                // подготавливаем место под вставку
+                mas[j + 1] = mas[j];
+                //уменьшаем индекс отсортированной части на 1
+                j --;
+                //вставляем элемент из неотсортированной части в отсортированную
+                mas[j + 1] = temp;
+            }
         }
+        return mas;
+    }
 
-        System.out.println();
-        System.out.println(deltaX);
+    public static void print(int[] mas) {
+        for (Integer i : mas) {
+            System.out.print(i + " ");
+        }
     }
 }
